4c751ba96f6756dc511116507c0e9c1e001fd06f
Rio Ramadhan <ramadhanar.rio@gmail.com>
2022-01-19 16:45:41 +0700

    fix test login dosen

--- application/controllers/C_loginDosen.php
+++ application/controllers/C_loginDosen.php
@@ -152,85 +152,25 @@ class C_loginDosen extends CI_Controller
 
     }
 
-    //  function authuser(){
-    //       $username=htmlspecialchars($this->input->post('username',TRUE),ENT_QUOTES);
-    //       $password=htmlspecialchars($this->input->post('password',TRUE),ENT_QUOTES);
+    public function authuser2()
+    {
+        $url = "https://apicybercampus.unair.ac.id/api/token/ambil-token-v2";
 
-    //       $url = "https://uacc.unair.ac.id/api/auth/login"; // The POST URL
-    //       // The POST Data
-    //       $postdata  = "LoginForm[username]=".$username;
-    //       $postdata .= "&LoginForm[password]=".$password;
+        $postdata = "user=sdm_studilanjut";
+        $postdata .= "&key=sdM@Stud1l@njut@582";
 
-    //       // print_r($postdata);
-    //       // die;
+        $ch = curl_init($url);
 
-    //       $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-    //       // Set the request type to POST
-    //       curl_setopt($ch, CURLOPT_POST, true);
-    //       // Pass the post parameters as a naked string
-    //       curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+        $result = curl_exec($ch);
+        $res = json_decode($result, true);
 
-    //       // Option to Return the Result, rather than just true/false
-    //       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        var_dump($res); die;
 
-    //       // Perform the request, and save content to $result
-    //       $result = curl_exec($ch);
-    //       $res = json_decode($result,true);
-
-    //       // Show the result?
-
-    //       //print_r($result);
-    //       //echo "<pre>";
-    // //die;
-    // $sta = $res['status'];
-
-    // $dataken = $res['data'];
-    //       echo "<pre>";
-    //       //echo "status=".$sta;
-    //       //print_r ($sta);
-    //       //print_r ($dataken);
-    //       $token = $res['data']['token'];
-    //       $nik = $res['data']['username'];
-    //       $namauser = $res['data']['name'];
-
-    // $this->session->set_userdata('token',$token);
-    // $this->session->set_userdata('NIK',$nik);
-    // $this->session->set_userdata('Nama',$namauser);
-
-    // $url = "https://uacc.unair.ac.id/api/view-beasiswa-sdm?access-token=".$token."&USERNAME=".$nik;
-
-    // $json = file_get_contents($url);
-
-    // $data1 = json_decode($json,true);
-
-    // $items = $data1['items'];
-
-    // if ($sta=="success") {
-
-    //       $cek_user=$this->m_login->auth_user($nik);
-    //       //foreach ($items as $user) {
-    //       if($cek_user->num_rows() > 0){ //jika login sebagai admin
-    //       //if(isset($user)){ //jika login sebagai admin
-    //             $data=$cek_user->row_array();
-    //                   $this->session->set_userdata('masuk',TRUE);
-    //                   $this->session->set_userdata('akses','1');
-    //             redirect('home');
-
-    //       }else{  // jika username dan password tidak ditemukan atau salah
-    //                     // $url=base_url();
-    //                     // echo $this->session->set_flashdata('msg','Username Atau Password Salah');
-    //                     // redirect($url);
-
-    //                           echo "gagal";
-    //             }
-    // }else{  // jika username dan password tidak ditemukan atau salah
-    //                     $url=base_url();
-    //                     echo $this->session->set_flashdata('msg','Username Atau Password Salah');
-    //                     redirect($url);
-    //             }
-
-    //   }
+    }
 
     public function authuser()
     {
